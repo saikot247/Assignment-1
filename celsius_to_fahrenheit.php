3e543cc734e4f8752a8828504d1d14f75567f5b4
Saikot Biswas <84744165+saikot247@users.noreply.github.com>
2024-07-09 01:30:25 +0900

    Add files via upload

---
+++ celsius_to_fahrenheit.php
@@ -0,0 +1,11 @@
+<?php
+
+$celsius = 32;
+
+ //celsius to fahrenheit conversion
+
+ $fahrenheit = ($celsius * 9/5) + 32;
+
+ printf("The temperature in fahrenheit is : %.2f degrees.", $fahrenheit);
+
+?>
