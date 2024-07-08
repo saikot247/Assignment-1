4166754a1bfcbabdfbb59a153ab12c708ae518a7
Saikot Biswas <84744165+saikot247@users.noreply.github.com>
2024-07-09 01:29:47 +0900

    Add files via upload

---
+++ largest_number.php
@@ -0,0 +1,17 @@
+<?php 
+
+$num1 = 4;
+$num2 = 5;
+$num3 = 6;
+
+//Find the largest number using if-else statements
+
+if ($num1>=$num2 && $num1>=$num3) {
+    echo "The largest number is $num1.";
+} else if ($num2>=$num2 && $num2>=$num3) {
+    echo "The largest number is $num2.";
+} else {
+    echo "The largest number is $num3.";
+}
+
+?>
